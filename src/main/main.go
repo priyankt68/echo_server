f2266fd076d50783cce51eab965514d7a4a0a27c
Priyank Trivedi <priyankt68@gmail.com>
2017-01-22 16:02:51 +0530

    Initial commit for echo webserver

--- src/main/main.go
+++ src/main/main.go
@@ -2,8 +2,19 @@ package main
 
 import(
 	"fmt"
+	"net/http"
+	"github.com/labstack/echo"
 )
 
 func main() {
 	fmt.Println("Yallo for the first line")
+
+	e := echo.New()
+
+	e.GET("/", func (c echo.Context) error {
+		return c.String(http.StatusOK, "Yallow from the web side!")
+		
+	})
+
+	e.Start(":8000")
 }
